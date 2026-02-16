296303772d5cc13ab124660c2a5a6b95f9fcce36
Muzamil223 <muzamil987614@gmail.com>
2026-02-17 04:42:10 +0500

    url fix

--- Api.Config.js
+++ Api.Config.js
@@ -1,5 +1,5 @@
 // Make sure this points to your backend
-export const BASE_API_URL = "http://localhost:8080";
+export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
 
 // Add a test function to verify connection
 export const testApiConnection = async () => {
